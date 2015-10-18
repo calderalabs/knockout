2b5123a1028e6812c1a0004b011c5298f3cedbe7
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-10-18 11:29:14 +0100

    Remove block usage test

--- tests/integration/pods/components/cl-tournament/component-test.js
+++ tests/integration/pods/components/cl-tournament/component-test.js
@@ -6,21 +6,10 @@ moduleForComponent('cl-tournament', 'Integration | Component | cl tournament', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
-
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{cl-tournament}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#cl-tournament}}
-      template block text
-    {{/cl-tournament}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
