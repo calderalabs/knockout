bebd76671d1d625597adfac26f300b7b217a4d55
Matteo Depalo <matteodepalo@gmail.com>
2015-11-08 18:00:41 +0000

    Remove animate.css in favor of copy pasting

--- app/pods/components/ko-filter/component.js
+++ app/pods/components/ko-filter/component.js
@@ -2,16 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'span',
-  classNameBindings: 'selectedClasses',
+  classNameBindings: 'selected',
   selected: false,
 
   click() {
     this.toggleProperty('selected');
-  },
-
-  selectedClasses: Ember.computed('selected', function() {
-    if (this.get('selected')) {
-      return 'selected animated pulse'
-    }
-  })
+  }
 });
