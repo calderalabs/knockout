d5c954681b64faa604d7499f09bc65f3b630c9a9
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-11-22 17:02:33 +0000

    Refine colors

--- app/models/match-group.js
+++ app/models/match-group.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const { Model, belongsTo, hasMany, attr } = DS;
+const { computed } = Ember;
 
 export default Model.extend({
   tournament: belongsTo('tournament'),
@@ -8,5 +10,13 @@ export default Model.extend({
   teamTwo: belongsTo('team'),
   matches: hasMany('matches'),
   bestOf: attr('number'),
-  elimination: attr('boolean')
+  elimination: attr('boolean'),
+  
+  teamOneScore: computed('matches.@each.winner', function() {
+    return this.get('matches').filterBy('winner', this.get('teamOne')).length;
+  }),
+  
+  teamTwoScore: computed('matches.@each.winner', function() {
+    return this.get('matches').filterBy('winner', this.get('teamTwo')).length;
+  })
 });
