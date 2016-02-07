8bc7b51a8b916817c8c2285aa549b30b3ba24939
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-02-07 17:23:42 +0000

    Update ember-cli

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  const ENV = {
+  var ENV = {
     modulePrefix: 'knockout',
     podModulePrefix: 'knockout/pods',
     environment: environment,
@@ -20,33 +20,15 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-
-    ENV['ember-cli-mirage'] = {
-      enabled: false
-    }
-  }
-
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
-
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
-
-  }
+  if (environment === 'production') {}
 
   return ENV;
 };
