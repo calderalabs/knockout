6ab646b1e2f8be44b80b61c71a2f20d4ea4cc6b0
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-02-08 22:48:20 +0000

    Add filtering

--- app/pods/components/ko-tournament-list/component.js
+++ app/pods/components/ko-tournament-list/component.js
@@ -1,10 +1,22 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
   tagName: 'section',
   classNames: ['ko-tournament-list'],
+  activeGame: null,
   tournaments: null,
-  onShow: null
+  onShow: null,
+
+  arrangedTournaments: computed('tournaments.@each.game', 'activeGame', function() {
+    const activeGame = this.get('activeGame');
+    const tournaments = this.get('tournaments');
+
+    if (activeGame) {
+      return tournaments.filterBy('game', this.get('activeGame'))
+    } else {
+      return tournaments;
+    }
+  })
 });
--- app/pods/tournaments/controller.js
+++ app/pods/tournaments/controller.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
+  queryParams: ['game'],
+  game: null,
+
   actions: {
     show(tournament) {
       this.transitionToRoute('tournament', tournament.get('id'));
