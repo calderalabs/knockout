7deb004af024367d6f30552091a7d492e096a7c6
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-02-15 14:17:52 +0000

    Fix sorting algorithm

--- app/pods/components/ko-tournament/component.js
+++ app/pods/components/ko-tournament/component.js
@@ -26,38 +26,51 @@ export default Component.extend({
     return this.get('sortingOptions').findBy('id', this.get('sortBy'));
   }),
 
-  timeline: computed(
-    '_sortedMatches.@each.matchGroup.startAt',
-    'tournament.matchGroups.@each.id', function() {
-    const matchGroups = this.get('tournament.matchGroups');
+  timeline: computed('_sortedMatches', function() {
+    const timeline = [];
+    let lastDayDate;
+    let lastMatchGroup
+    let currentDay;
+    let currentCollection;
 
-    const matchesByDay = _.groupBy(this.get('_sortedMatches'), function(match) {
-      return moment(match.get('matchGroup.startAt')).startOf('day').toDate();
+    this.get('_sortedMatches').forEach(function(match) {
+      const matchGroup = match.get('matchGroup');
+      const dayDate = moment(matchGroup.get('startAt')).startOf('day').toDate();
+
+      if (!lastDayDate || dayDate.getTime() !== lastDayDate.getTime()) {
+        lastDayDate = dayDate;
+
+        currentDay = {
+          title: moment(dayDate).calendar(null, {
+            sameDay: '[Today]',
+            nextDay: '[Tomorrow]',
+            nextWeek: '[Next] dddd',
+            lastDay: '[Yesterday]',
+            lastWeek: '[Last] dddd',
+            sameElse: 'DD MMMM YYYY'
+          }),
+
+          collections: []
+        };
+
+        timeline.pushObject(currentDay);
+      }
+
+      if (matchGroup !== lastMatchGroup) {
+        lastMatchGroup = matchGroup;
+
+        currentCollection = {
+          matchGroup: matchGroup,
+          matches: []
+        }
+
+        currentDay.collections.pushObject(currentCollection);
+      }
+
+      currentCollection.matches.pushObject(match);
     });
 
-    return keys(matchesByDay).map(function(day) {
-      const matchesByGroupId = _.groupBy(matchesByDay[day], function(match) {
-        return match.get('matchGroup.id');
-      });
-
-      return {
-        title: moment(day).calendar(null, {
-          sameDay: '[Today]',
-          nextDay: '[Tomorrow]',
-          nextWeek: '[Next] dddd',
-          lastDay: '[Yesterday]',
-          lastWeek: '[Last] dddd',
-          sameElse: 'DD MMMM YYYY'
-        }),
-
-        collections: keys(matchesByGroupId).map(function(id) {
-          return {
-            matchGroup: matchGroups.findBy('id', id),
-            matches: matchesByGroupId[id]
-          };
-        })
-      };
-    });
+    return timeline;
   }),
 
   _matchesSorting: computed('sortBy', function() {
