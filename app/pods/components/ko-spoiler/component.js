3e1541eb9dd999b371e70215a2261035a95fd4ce
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-02-15 15:16:33 +0000

    Add spoiler component

---
+++ app/pods/components/ko-spoiler/component.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+const { Component } = Ember;
+
+export default Component.extend({
+  classNameBindings: [':ko-spoiler', 'isRevealed:ko-spoiler--revealed'],
+  title: 'Show Spoiler',
+  isRevealed: false,
+
+  actions: {
+    reveal(event) {
+      event.stopPropagation();
+      event.preventDefault();
+      this.set('isRevealed', true);
+    }
+  }
+})
