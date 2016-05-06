2893f8abe3c33caeef48672ab7f825287532fd06
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-05-06 13:28:54 +0100

    Remove usage of concat

--- app/pods/components/ko-player/component.js
+++ app/pods/components/ko-player/component.js
@@ -10,10 +10,12 @@ export default Component.extend(VelocityMixin, {
   player: null,
   teamOneFullName: computed.readOnly('_matchGroup.teamOne.fullName'),
   teamTwoFullName: computed.readOnly('_matchGroup.teamTwo.fullName'),
-  stage: computed.readOnly('_matchGroup.stage'),
-  tournamentName: computed.readOnly('_matchGroup.tournament.name'),
   matchNumber: computed.readOnly('player.match.number'),
   _matchGroup: computed.readOnly('player.match.matchGroup'),
+
+  title: computed('_matchGroup.stage', '_matchGroup.tournament.name', function() {
+    return `${this.get('_matchGroup.stage')}, ${this.get('_matchGroup.tournament.name')}`;
+  }).readOnly(),
 
   animateOpacity: on('didInsertElement', function() {
     this.animate({ opacity: 1 }, TRANSITION_DURATION);
