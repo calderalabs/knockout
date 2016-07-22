ce92332a8bcfd294e0983b8d9129f26e00ecaef7
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-07-22 13:20:41 +0100

    Always reload collections

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,13 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 const { JSONAPIAdapter } = DS;
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'simple-auth-authorizer:jwt'
+  authorizer: 'simple-auth-authorizer:jwt',
+
+  shouldBackgroundReloadAll() {
+    return false;
+  },
+
+  shouldReloadAll() {
+    return true;
+  }
 });
