c7301b6d0d290c05b941738c3b9f79231b09e520
Eugenio Depalo <eugeniodepalo@gmail.com>
2016-07-25 20:17:41 +0100

    Move assets to assets folder

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,17 +14,23 @@ module.exports = function(defaults) {
         'bower_components/neat/app/assets/stylesheets',
         'bower_components/font-awesome/scss'
       ]
+    },
+
+    favicon: {
+      config: {
+        path: '/assets/favicons'
+      }
     }
   });
 
   ['eot', 'svg', 'ttf', 'woff', 'woff2'].forEach(function(ext) {
     app.import('bower_components/font-awesome/fonts/fontawesome-webfont.' + ext, {
-      destDir: '/fonts'
+      destDir: '/assets/fonts'
     });
   });
 
-  app.import('bower_components/font-awesome/fonts/FontAwesome.otf', { destDir: '/fonts' });
-  app.import('bower_components/css-social-buttons/css/zocial.otf', { destDir: '/fonts' });
+  app.import('bower_components/font-awesome/fonts/FontAwesome.otf', { destDir: '/assets/fonts' });
+  app.import('bower_components/css-social-buttons/css/zocial.otf', { destDir: '/assets/fonts' });
   app.import('bower_components/css-social-buttons/css/zocial.css');
 
   return app.toTree();
